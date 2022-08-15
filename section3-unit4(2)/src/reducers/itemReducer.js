1aa85aca5d7c36ac9723c6c210589d5ec8a6bb20
kanghyew0n <2018314001@tukorea.ac.kr>
2022-08-15 22:16:02 +0900

    section3-unit4 추가

--- section3-unit4(2)/src/reducers/itemReducer.js
+++ section3-unit4(2)/src/reducers/itemReducer.js
@@ -2,24 +2,35 @@ import { REMOVE_FROM_CART, ADD_TO_CART, SET_QUANTITY } from "../actions/index";
 import { initialState } from "./initialState";
 
 const itemReducer = (state = initialState, action) => {
-
   switch (action.type) {
     case ADD_TO_CART:
-      //TODO
+      return Object.assign({}, state, {
+        cartItems: [...state.cartItems, action.payload],
+      });
 
       break;
     case REMOVE_FROM_CART:
-      //TODO
+      const filtered = state.cartItems.filter(
+        (item) => item.itemId !== action.payload.itemId
+      );
+      return Object.assign({}, state, {
+        cartItems: [...filtered],
+      });
 
       break;
     case SET_QUANTITY:
-      let idx = state.cartItems.findIndex(el => el.itemId === action.payload.itemId)
-      //TODO
+      let idx = state.cartItems.findIndex(
+        (el) => el.itemId === action.payload.itemId
+      );
+      state.cartItems[idx].quantity = action.payload.quantity;
+      return Object.assign({}, state, {
+        cartItems: [...state.cartItems],
+      });
 
       break;
     default:
       return state;
   }
-}
+};
 
 export default itemReducer;
